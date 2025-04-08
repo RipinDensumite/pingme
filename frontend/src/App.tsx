f57ddccfa5a471b5b8792e4e258ae628850d0c75
Ripin <52118329+RipinDensumite@users.noreply.github.com>
2025-04-08 20:34:06 +0800

    initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="min-h-[100dvh] flex items-center justify-center bg-gray-100">
+      <h1>Hello World</h1>
+    </section>
+  );
+}
+
+export default App;
